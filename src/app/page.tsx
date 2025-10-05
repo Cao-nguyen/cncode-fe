d6964cb7c40675237e353d45ef2f54855619b0ff
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-10-05 09:15:36 +0700

    update 5.10.2025

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1 className="uppercase text-center m-[20px] text-[#FF883E] font-bold text-[50px]">Xin chào, đây là dự án CNcode - Nền tảng dạy và học công nghệ thông tin trực tuyến do Lý Cao Nguyên phát triển</h1>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,10 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   mode: "jit",
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
