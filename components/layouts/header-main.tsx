425153fbb5de7b95d74896cdd14ffc8b06179d17
Cao-nguyen <chinhphucnguvan@gmail.com>
2026-03-01 21:31:08 +0700

    Update header-main.tsx

--- components/layouts/header-main.tsx
+++ components/layouts/header-main.tsx
@@ -11,10 +11,6 @@ export function Header() {
     const pathname = usePathname()
 
     const { theme, setTheme } = useTheme();
-    const [mounted, setMounted] = useState(false);
-
-    useEffect(() => setMounted(true), []);
-    if (!mounted) return null;
 
     const menu = [
         {
