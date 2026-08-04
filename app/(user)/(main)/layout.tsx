88185d10c82e280dd94ea4609acb86fc7a5da436
Cao-nguyen <chinhphucnguvan@gmail.com>
2026-08-04 21:06:40 +0700

    Update layout.tsx

--- app/(user)/(main)/layout.tsx
+++ app/(user)/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function UserLayoutMain({
     return (
         <>
             <Header />
-            <div className="min-h-screen mt-0 md:mt-0 lg:mt-15">
+            <div className="min-h-[calc(100dvh-96px)] lg:min-h-screen mt-0 md:mt-0 lg:mt-15">
                 {children}
             </div>
             <Footer />
